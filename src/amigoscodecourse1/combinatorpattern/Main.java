5a028dc1333ed5e86a6a2d80fb63cf4fd0addeae
muradisgandar <murad.isgandar@gmail.com>
2021-02-05 23:10:07 +0400

    new samples

---
+++ src/amigoscodecourse1/combinatorpattern/Main.java
@@ -0,0 +1,18 @@
+package amigoscodecourse1.combinatorpattern;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer(
+                "Alice",
+                "alice@gmail.com",
+                "+0898321314",
+                LocalDate.of(2000,1,1)
+        );
+
+        System.out.println(new CustomerValidatorService().isValid(customer));
+
+    }
+}
