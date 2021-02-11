db629181c0f3b9ebd282d6b96940ed2be84cae6a
muradisgandar <murad.isgandar@gmail.com>
2021-02-12 00:32:19 +0400

    min max

--- amigoscodestreams2/src/main/java/lectures/Lecture3.java
+++ amigoscodestreams2/src/main/java/lectures/Lecture3.java
@@ -8,24 +8,25 @@ import java.util.List;
 
 public class Lecture3 {
 
-  public void min() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    public static void main(String[] args) throws Exception {
+        min();
+        max();
+    }
 
-    Integer min = numbers.stream()
-        .min(Comparator.naturalOrder())
-        .get();
+    public static void min() throws Exception {
+        final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
-    System.out.println(min);
+        Integer min = numbers.stream()
+                .min(Comparator.naturalOrder()).get();
+        System.out.println(min);
 
-  }
+    }
 
-  public void max() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    public static void max() throws Exception {
+        final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
-    Integer max = numbers.stream()
-        .max(Comparator.naturalOrder())
-        .get();
-
-    System.out.println(max);
-  }
+        Integer max = numbers.stream()
+                .max(Comparator.naturalOrder()).get();
+        System.out.println(max);
+    }
 }
