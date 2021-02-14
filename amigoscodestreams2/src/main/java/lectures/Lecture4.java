3deb158842c8de66ae562ed51d274d56e420ab66
muradisgandar <murad.isgandar@gmail.com>
2021-02-14 17:56:13 +0400

    filtering

--- amigoscodestreams2/src/main/java/lectures/Lecture4.java
+++ amigoscodestreams2/src/main/java/lectures/Lecture4.java
@@ -10,24 +10,30 @@ import java.util.stream.Collectors;
 
 public class Lecture4 {
 
-  public void distinct() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+    public static void main(String[] args) throws Exception {
+        distinct();
+        distinctWithSet();
+    }
 
-    List<Integer> distinctNumbers = numbers.stream()
-        .distinct()
-        .collect(Collectors.toList());
+    public static void distinct() throws Exception {
+        final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+
+        List<Integer> distinctList = numbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        System.out.println(distinctList);
 
 
-    System.out.println(distinctNumbers);
 
-  }
+    }
 
-  public void distinctWithSet() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+    public static void distinctWithSet() throws Exception {
+        final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
 
-    Set<Integer> distinctNumbers = numbers.stream()
-        .collect(Collectors.toSet());
+        Set<Integer> distinctSet = numbers.stream()
+                .collect(Collectors.toSet());
 
-    System.out.println(distinctNumbers);
-  }
+        System.out.println(distinctSet);
+    }
 }
--- amigoscodestreams2/src/main/java/lectures/Lecture5.java
+++ amigoscodestreams2/src/main/java/lectures/Lecture5.java
@@ -14,47 +14,39 @@ import java.util.stream.Collectors;
 
 public class Lecture5 {
 
-  public void understandingFilter() throws Exception {
-    ImmutableList<Car> cars = MockData.getCars();
+    public static void main(String[] args) throws Exception {
+        understandingFilter();
+        ourFirstMapping();
+        averageCarPrice();
+        test();
+    }
 
-    final Predicate<Car> carPredicate = car -> car.getPrice() < 20000;
+    public static void understandingFilter() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
+        List<Car> carList = cars.stream()
+                .filter(car -> car.getPrice() < 10000)
+                .collect(Collectors.toList());
 
-    List<Car> carsFiltered = cars.stream()
-        .filter(carPredicate)
-        .collect(Collectors.toList());
+        carList.forEach(System.out::println);
 
-    carsFiltered.forEach(System.out::println);
-    System.out.println(carsFiltered.size());
-  }
+    }
 
 
-  public void ourFirstMapping() throws Exception {
-    // transform from one data type to another
-    List<Person> people = MockData.getPeople();
+    public static void ourFirstMapping() throws Exception {
+        // transform from one data type to another
+        List<Person> people = MockData.getPeople();
 
-    List<PersonDTO> dtos = people.stream()
-        .map(PersonDTO::map)
-        .collect(Collectors.toList());
 
-    dtos.forEach(System.out::println);
+    }
 
-    System.out.println(dtos.size());
+    public static void averageCarPrice() throws Exception {
+        // calculate average of car prices
 
-  }
+    }
 
-  public void averageCarPrice() throws Exception {
-    // calculate average of car prices
-    double average = MockData.getCars()
-        .stream()
-        .mapToDouble(Car::getPrice)
-        .average()
-        .orElse(0);
-    System.out.println(average);
-  }
+    public static void test() throws Exception {
 
-  public void test() throws Exception {
-    MockData.getCars().forEach(System.out::println);
-  }
+    }
 }
 
 
