475ed810eae55dd3a3619b235e5e5dfe66a59f24
muradisgandar <murad.isgandar@gmail.com>
2021-03-06 11:06:35 +0400

    map operations

--- amigoscodestreams2/src/main/java/lectures/Lecture5.java
+++ amigoscodestreams2/src/main/java/lectures/Lecture5.java
@@ -8,6 +8,7 @@ import com.google.common.collect.ImmutableList;
 import mockdata.MockData;
 
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -35,12 +36,23 @@ public class Lecture5 {
     public static void ourFirstMapping() throws Exception {
         // transform from one data type to another
         List<Person> people = MockData.getPeople();
-
+        people.stream()
+//                .map(person -> new PersonDTO(person.getId(), person.getFirstName(), person.getAge()))
+                .map(PersonDTO::map)   // is defined in PersonDTO
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
 
     }
 
     public static void averageCarPrice() throws Exception {
         // calculate average of car prices
+        double average = MockData.getCars()
+                .stream()
+                .mapToDouble(Car::getPrice)
+                .average()
+                .orElse(0);
+
+        System.out.println(average);
 
     }
 
