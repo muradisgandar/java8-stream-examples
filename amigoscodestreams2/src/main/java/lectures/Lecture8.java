0de845df341eafca4983a2d12ba3f6123ae6c8da
muradisgandar <murad.isgandar@gmail.com>
2021-03-08 21:02:25 +0400

    groping

--- amigoscodestreams2/src/main/java/lectures/Lecture8.java
+++ amigoscodestreams2/src/main/java/lectures/Lecture8.java
@@ -9,39 +9,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Lecture8 {
 
-  public void simpleGrouping() throws Exception {
-    Map<String, List<Car>> grouping = MockData.getCars()
-        .stream()
-        .collect(Collectors.groupingBy(Car::getMake));
+    public static void main(String[] args) throws Exception {
+        simpleGrouping();
+        groupingAndCounting();
+    }
 
-    grouping.forEach((make, cars) -> {
-      System.out.println(make);
-      cars.forEach(System.out::println);
-    });
-  }
+    public static void simpleGrouping() throws Exception {
+        Map<String, List<Car>> grouping = MockData.getCars()
+                .stream()
+                .collect(Collectors.groupingBy(Car::getMake));
 
-  public void groupingAndCounting() throws Exception {
-    ArrayList<String> names = Lists
-        .newArrayList(
-            "John",
-            "John",
-            "Mariam",
-            "Alex",
-            "Mohammado",
-            "Mohammado",
-            "Vincent",
-            "Alex",
-            "Alex"
-        );
+        grouping.forEach((make, cars) -> {
+            System.out.println(make);
+            cars.forEach(System.out::println);
+        });
 
-    Map<String, Long> counting = names.stream()
-        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-    counting.forEach((name, count) -> System.out.println(name + " > " + count));
-  }
+    }
+
+    public static void groupingAndCounting() throws Exception {
+        ArrayList<String> names = Lists
+                .newArrayList(
+                        "John",
+                        "John",
+                        "Mariam",
+                        "Alex",
+                        "Mohammado",
+                        "Mohammado",
+                        "Vincent",
+                        "Alex",
+                        "Alex"
+                );
+
+        Map<String, Long> counting = names.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        counting.forEach((name ,count) -> System.out.println(name + " count is " + count));
+
+
+    }
 
 }
